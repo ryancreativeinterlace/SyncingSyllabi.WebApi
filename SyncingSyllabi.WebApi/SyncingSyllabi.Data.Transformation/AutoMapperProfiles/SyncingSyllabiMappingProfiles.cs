bfa95032f4dd25c94a9b4fe63db7be7a8cc37f0c
RyanOctalTech <ryan.juanir@octaltech.net>
2022-02-06 17:21:59 +0800

    Partial Commit for Class Libraries

---
+++ SyncingSyllabi.WebApi/SyncingSyllabi.Data.Transformation/AutoMapperProfiles/SyncingSyllabiMappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncingSyllabi.Data.Transformation.AutoMapperProfiles
+{
+    public class SyncingSyllabiMappingProfiles : Profile
+    {
+        public SyncingSyllabiMappingProfiles()
+        {
+            // Sample
+            // CreateMap<CampaignDeliveryDto, CampaignDeliveryModel>(MemberList.None).ReverseMap();
+        }
+    }
+}
