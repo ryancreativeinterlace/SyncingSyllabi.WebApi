3eabbedf5391d324cd11e214c29524af7dc63da8
RyanSteve07 <ryan.juanir@octaltech.net>
2022-02-15 11:54:47 +0800

    Update Added PaginationDto

---
+++ SyncingSyllabi.WebApi/SyncingSyllabi.Data/Dtos/Core/PaginatedResultDto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SyncingSyllabi.Data.Dtos.Core
+{
+    public class PaginatedResultDto<TDto>
+    {
+        public PaginatedResultDto()
+        {
+            Items = Enumerable.Empty<TDto>();
+        }
+
+        public IEnumerable<TDto> Items { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+        public int TotalCount { get; set; }
+
+        public PaginatedResultDto<TSelectDTO> Select<TSelectDTO>(Func<TDto, TSelectDTO> selector)
+        {
+            var newPagedDTO = new PaginatedResultDto<TSelectDTO>();
+            newPagedDTO.Items = this.Items
+                                        .Select(selector)
+                                        .ToList();
+            newPagedDTO.Skip = this.Skip;
+            newPagedDTO.Take = this.Take;
+            newPagedDTO.TotalCount = this.TotalCount;
+
+            return newPagedDTO;
+        }
+
+        public static PaginatedResultDto<TDto> FromPaginatedItems(IEnumerable<TDto> items, int totalCount, int skip = 0, int take = 0)
+        {
+            var newPagedDTO = new PaginatedResultDto<TDto>();
+            newPagedDTO.Items = items;
+            newPagedDTO.Skip = skip;
+            newPagedDTO.Take = take;
+            newPagedDTO.TotalCount = totalCount;
+
+            return newPagedDTO;
+        }
+    }
+}
---
+++ SyncingSyllabi.WebApi/SyncingSyllabi.Data/Dtos/Core/PaginationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncingSyllabi.Data.Dtos.Core
+{
+    public class PaginationDto
+    {
+        public bool IncludeTotal { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
