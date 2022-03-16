d2e1be9cebb07f3b314da6f8b28258444bdcb25c
RyanSteve07 <ryan.juanir@octaltech.net>
2022-03-16 09:29:22 +0800

    Update AppSettings new Config

--- SyncingSyllabi.WebApi/SyncingSyllabi.Data/Constants/AwsConstants.cs
+++ SyncingSyllabi.WebApi/SyncingSyllabi.Data/Constants/AwsConstants.cs
@@ -6,7 +6,7 @@ namespace SyncingSyllabi.Data.Constants
 {
     public class AwsConstants
     {
-        public const string AKI = "QUtJQVpCNFAzVlJQSUZCS01NSzQ=";
-        public const string SAK = "eVk1NkF4TG55QTJxNGk1MjNkR0FRVnhtaDRJRXVyZ2ZKOGVwcERXTA==";
+        public const string AKI = "QUtJQVpCNFAzVlJQTlVZRVI0VVE=";
+        public const string SAK = "SWxWaVZwKytmR3BsWGFnMHNXRE1hYU9UQlpNRE0vMnFwdGFrY1kzcg==";
     }
 }
